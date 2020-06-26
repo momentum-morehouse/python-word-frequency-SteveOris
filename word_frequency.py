7630affd9e038db0cf2bae58fe53cdd4573ad5e9
Steve Orisakwe <Steveoris@parkrowmedia.com>
2020-06-26 02:46:16 +0000

    its ugly but it works lol im going to work on it overnight

--- word_frequency.py
+++ word_frequency.py
@@ -1,19 +1,45 @@
+import re
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+def flatten_lol(lol):
+  flat_list = []
+  for l in lol:
+    for word in l:
+      flat_list.append(word)
+  return flat_list
+
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    # Your code will go here. You can write additional functions if you want, and call them inside this function.
-    # first open the file
-    with open(file) as f:
-        lyrics = f.readlines()
-        word_list = [line.split() for line in lyrics]
-        print(word_list)
 
+    wordfreq = []
+    bhop = []
+    
+    with open(file) as f:
+      for items in f:
+          bhop.append(items)
+      cleaned_text = []
+      for w in bhop:
+        clean = re.sub(r"[!?.,]","", w.lower())
+        cleaner = clean.split()
+        cleaned_text.append(cleaner)
+    
+      working_list = flatten_lol(cleaned_text)
+      for word in list(working_list):  
+          if word in STOP_WORDS:
+           working_list.remove(word)
+      for word in working_list:
+        wordfreq.append(working_list.count(word))
+        print("Pairs\n" + str(list(zip(working_list, wordfreq))))
+    
+
+
+  
 # This is an "incantation." You will not see it very often, and it needs to be here to be able to pass file names as arguments.
 if __name__ == "__main__":
     import argparse
